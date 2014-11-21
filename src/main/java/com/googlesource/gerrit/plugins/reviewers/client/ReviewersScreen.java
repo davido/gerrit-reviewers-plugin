88bab568f7a4487162363dc5a078fcb2b58636b6
Simon Lei <simon.lei@ericsson.com>
2014-11-21 15:20:54 -0500

    Add suggestions for groups when user types in reviewer box
    When a user types in the reviewer box in the reviewer screen, suggestions
    for groups to add will show up.
    
    Change-Id: I0a8a5897fe08935631bb70d7f19b67b99b26d45c

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/client/ReviewersScreen.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/client/ReviewersScreen.java
@@ -18,6 +18,7 @@ import com.google.gerrit.client.rpc.NativeMap;
 import com.google.gerrit.client.rpc.Natives;
 import com.google.gerrit.plugin.client.rpc.RestApi;
 import com.google.gerrit.plugin.client.screen.Screen;
+import com.google.gerrit.plugin.client.ui.GroupSuggestOracle;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -28,6 +29,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwtexpui.globalkey.client.NpTextBox;
 
@@ -129,7 +131,9 @@ public class ReviewersScreen extends HorizontalPanel {
 
   Panel createInputPanel(){
     final NpTextBox filterBox = new NpTextBox();
-    final NpTextBox reviewerBox = new NpTextBox();
+    // TODO(davido): Remove hard coded start suggest char 3
+    final GroupSuggestOracle oracle = new GroupSuggestOracle(3);
+    final SuggestBox reviewerBox = new SuggestBox(oracle, new NpTextBox());
     filterBox.getElement().setPropertyString("placeholder", "filter");
     reviewerBox.getElement().setPropertyString("placeholder", "reviewer");
 
