52ebd8ca5e205f484070703e972826a7056f7099
David Pursehouse <david.pursehouse@sonymobile.com>
2016-02-05 14:34:26 +0900

    Add labels on the input widgets
    Change-Id: I9ff68eed789982a1457719456033094519970bef

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/client/ReviewersScreen.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/client/ReviewersScreen.java
@@ -24,6 +24,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
@@ -129,10 +130,17 @@ public class ReviewersScreen extends HorizontalPanel {
   }
 
   Panel createInputPanel(){
+    Grid inputGrid = new Grid(2, 2);
+
     final NpTextBox filterBox = new NpTextBox();
-    final NpTextBox reviewerBox = new NpTextBox();
     filterBox.getElement().setPropertyString("placeholder", "filter");
+    inputGrid.setText(0, 0, "Filter: ");
+    inputGrid.setWidget(0, 1, filterBox);
+
+    final NpTextBox reviewerBox = new NpTextBox();
     reviewerBox.getElement().setPropertyString("placeholder", "reviewer");
+    inputGrid.setText(1, 0, "Reviewer: ");
+    inputGrid.setWidget(1, 1, reviewerBox);
 
     Button addButton = new Button("Add");
     addButton.setStyleName("reviewers-addButton");
@@ -155,8 +163,7 @@ public class ReviewersScreen extends HorizontalPanel {
 
     Panel p = new VerticalPanel();
     p.setStyleName("reviewers-inputPanel");
-    p.add(filterBox);
-    p.add(reviewerBox);
+    p.add(inputGrid);
     p.add(addButton);
     return p;
   }
